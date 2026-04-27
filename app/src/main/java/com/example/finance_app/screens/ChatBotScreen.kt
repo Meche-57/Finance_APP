b27cd82509d0ac4612717cef596ea1d9b6d2aab5
Meche-57 <93935558+Meche-57@users.noreply.github.com>
2026-04-27 17:05:06 +0100

    Added UI chat bubbles in ChatBotScreen.kt for better visibility and added user disclaimer when using the ChatBotScreen.kt

--- app/src/main/java/com/example/finance_app/screens/ChatBotScreen.kt
+++ app/src/main/java/com/example/finance_app/screens/ChatBotScreen.kt
@@ -1,13 +1,18 @@
 package com.example.finance_app.screens
 
-import android.content.Context
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -20,14 +25,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.example.finance_app.AppDatabase
 import com.example.finance_app.budgetDao
 import com.example.finance_app.components.GeminiAI
 import com.example.finance_app.goalsDao
 import com.example.finance_app.spendingDao
+import com.example.finance_app.ui.theme.Card_Navy
 import kotlinx.coroutines.launch
+import com.example.finance_app.ui.theme.Purple_Card
 
 
 @Composable
@@ -49,11 +55,13 @@ fun ChatBotScreen(){
 
 
     LaunchedEffect(Unit) {
+
+        // all transactions from Room database
         val spendingList = spendingDao.getAll()
         val goalsList = goalsDao.getAll()
         val budgetList = budgetDao.getAll()
 
-
+// calculating total income, expenses and balance
         income = spendingList.filter { it.category == "Income" }
             .sumOf { it.amount }
         expenses = spendingList.filter { it.category!= "Income" }
@@ -66,36 +74,73 @@ fun ChatBotScreen(){
         goals = goalsList.sumOf{ it.current}
         remainingBudget = budget - expenses
 
+        messages = listOf("Bot: Hello, I'm FinanBot :D", "Bot: How can I help you with your finances?",
+            "Bot: Disclaimer:Responses may not be accurate, please use me as guidance only")
+
         }
 
 
+        Column(modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize()){
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-
-    ) {
-        Column(modifier = Modifier.padding(16.dp)) {
-
+            // Chatbot title
             Text("FinanApp ChatBot")
             Spacer(modifier = Modifier.height(16.dp))
+
+
+// Messages
 Column(
      modifier = Modifier
+         .weight(1f)
          .verticalScroll(rememberScrollState())) {
-    messages.forEach {
-        Text(text = it)
-        Spacer(modifier = Modifier.height(8.dp))
+
+    messages.forEach { message ->
+        val user = message.startsWith("You:")
+        val Disclaimer = message.startsWith("Bot: Disclaimer")
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = if (user) Arrangement.End else Arrangement.Start // Aligns messages to the end if User
+        ) {
+            Box(
+                modifier = Modifier
+
+                    .padding(8.dp)
+                    .background(
+                        when {
+                            user -> Purple_Card
+                            Disclaimer -> Color.Gray
+                            else -> Card_Navy// Background for bubble
+                        },
+                        shape = RoundedCornerShape(12.dp)
+                    )
+                    .padding(10.dp)
+
+            ) {
+                Text(
+                    text = message.replace("You:", "").replace("Bot:", "").replace("Disclaimer:", ""),
+                    color = Color.White
+                )
+            }
+        }
     }
+
+
+
+
     if (isLoading) {
         Text(text = "FinanBot is analysing...")
     }
+}
 
 
             Row{
          TextField(
              value = userInput,
              onValueChange = { userInput = it},
-             label = { Text("Type your message here") }
+             label = { Text("Type your message here") },
+             modifier = Modifier.weight(1f) // Keeps Input area at the bottom
 
 
          )
@@ -144,6 +189,6 @@ Column(
                 }
      }
 
-}}}
+}
 
 
